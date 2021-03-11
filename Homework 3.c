ad6be408c59cadc26837efdd63c678decb847ded
AndreaCeleste20 <78926336+AndreaCeleste20@users.noreply.github.com>
2021-03-11 13:36:47 -0600

    Add files via upload

---
+++ Homework 3.c
@@ -0,0 +1,82 @@
+
+#include <stdio.h>
+#define T 1
+#define F 0
+
+int
+main ()
+{
+  int x, n = F, o = F;
+
+  while ((x = getchar ()) != EOF)
+  {
+    switch (x)
+      {
+      case '(':
+	o = T;
+	putchar (x);
+	break;
+      case ')':
+	o = F;
+	putchar (x);
+	break;
+      case '<':
+	o = T;
+	putchar (x);
+	break;
+      case '>':
+	o = F;
+	putchar (x);
+	break;
+      case '{':
+	o = T;
+	putchar (x);
+	break;
+      case '}':
+	o = F;
+	putchar (x);
+	break;
+      case '[':
+	o = T;
+	putchar (x);
+	break;
+      case ']':
+	o = F;
+	putchar (x);
+	break;
+
+      default:
+	if (x != '.' && x != ':' && x != '\?' && x != '!' && x != '-'
+	    && x != ';' && x != '_')
+	  {
+	    if (o == T)
+	      {
+		putchar (x);
+	      }
+	    else
+	      {
+		if (x == '1' || x == '2' || x == '3' || x == '4' || x == '5'
+		    || x == '6' || x == '7' || x == '8' || x == '9'
+		    || x == '0')
+		  {
+		    n++;
+		  }
+		else
+		  {
+		    n = 0;
+		  }
+		if (n == 1)
+		  {
+		    putchar ('X');
+		  }
+		if (n == 0)
+		  {
+		    putchar (x);
+		  }
+	      }
+	  }
+      }
+
+  }
+  return 0;
+}
